b77c7a279d01349213579340af1ec27a009c4ab9
spiderasasen <spiderasasen@gmail.com>
2026-03-09 15:13:54 -0400

    made the input section of the code

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+from typing import List
+
+# reads the input and returns a list
+def input_list() -> List[str]:
+    string: str = ""
+    user_list: List[str] = []
+
+    while True:
+        user_input: str = input("Type Stop to finish your input.\nPlease enter a list of words: \n")
+        user_input: str = user_input.strip().lower()
+        if user_input != "stop":
+            user_list.append(user_input)
+        else:
+            break
+
+    return user_list
 
 def main():
-    print("Hello World")
+    user_list: List[str] = input_list()
+    print(user_list)
 
 if __name__ == '__main__':
     main()
